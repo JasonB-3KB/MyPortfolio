10f6058bac1a2fb4355be4e6aaa15cd3adb24edb
Jason Bagwill <mr.bagwill@gmail.com>
2021-05-18 19:14:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+         if(this.scrollY > 20){
+         $('.navbar').addClass("sticky");
+        } else{
+             $('.navbar').removeClass("sticky");
+        }
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Coder", "Content Creator", "3D Artist", "YouTuber"],
+        typeSpeed:100, 
+        backSpeed:40, 
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Coder", "Content Creator", "3D Artist", "YouTuber"],
+        typeSpeed:100, 
+        backSpeed:40, 
+        loop: true
+    });
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
